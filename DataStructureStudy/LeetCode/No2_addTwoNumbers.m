44d04604dadcb3bc001b85de047421b94bff852a
jolin.ding <jolin.ding@corp.to8to.com>
2020-03-20 19:02:06 +0800

    增加各种加法的逻辑

--- DataStructureStudy/LeetCode/No2_addTwoNumbers.m
+++ DataStructureStudy/LeetCode/No2_addTwoNumbers.m
@@ -8,6 +8,7 @@
 
 #import "No2_addTwoNumbers.h"
 #import "StructureDefine.h"
+#import "PrintDefine.h"
 
 @implementation No2_addTwoNumbers
 
@@ -18,6 +19,12 @@
         testTwoNumbersAdd();
         
         testTwoNumbersReverseAdd();
+        
+        testTwoIntegerAdd();
+        
+        testTwoStringAdd();
+        
+        testAddIntergerInArrayForm();
     }
     return self;
 }
@@ -53,18 +60,12 @@ ListNode * addTwoNumbers(ListNode * head1, ListNode * head2)
     ListNode * p3 = NULL;
     int carry = 0;
     
-    while (p1 || p2) {
+    while (p1 || p2 || carry) {
         int value1 = p1 ? p1->value : 0;
         int value2 = p2 ? p2->value : 0;
         int sum = value1 + value2 + carry;
-        if (sum >= 10) {
-            carry = 1;
-            sum %= 10;
-        }
-        else {
-            carry = 0;
-        }
-        ListNode * node = nodeFromValue(sum);
+        ListNode * node = nodeFromValue(sum % 10);
+        carry = sum / 10;
         if (!result) {
             result = node;
             p3 = node;
@@ -75,10 +76,6 @@ ListNode * addTwoNumbers(ListNode * head1, ListNode * head2)
         }
         p1 ? p1 = p1->next : 0;
         p2 ? p2 = p2->next : 0;
-    }
-    if (carry > 0) {
-        ListNode * node = nodeFromValue(carry);
-        p3->next = node;
     }
     
     return result;
@@ -96,7 +93,7 @@ void testTwoNumbersReverseAdd()
     ListNode * head2 = nodeFromArray(array2, size);
     
     ListNode * sum = addTwoReverseNumbers(head1, head2);
-    printf("\nreverse:\n");
+    printf("reverse:\n");
     printListNode(head1);
     printf(" + ");
     printListNode(head2);
@@ -168,9 +165,105 @@ ListNode * resultNode(ListNode * head1, ListNode * head2, int * carry)
     int value2 = head2->value;
     int sum = value1 + value2 + *carry;
     ListNode * node = nodeFromValue(sum % 10);
-    *carry = sum /= 10;
+    *carry = sum / 10;
     node->next = head;
     return node;
+}
+
+#pragma mark - 两数相加
+void testTwoIntegerAdd()
+{
+    int a = 1, b = -3;
+    int sum = addTwoIntegers(a, b);
+    printf("integer : %d + %d = %d\n", a, b, sum);
+}
+
+int addTwoIntegers(int a, int b)
+{
+    return b == 0 ? a : addTwoIntegers( a ^ b, (a & b) << 1);
+}
+
+
+void testTwoStringAdd()
+{
+    char * num1 = "99345";
+    char * num2 = "756";
+    char * sum = addTwoString(num1, num2);
+    printf("string : %s + %s = %s\n", num1, num2, sum);
+}
+
+char * addTwoString(char * num1, char * num2)
+{
+    if (!num1) return num2;
+    if (!num2) return num1;
+    
+    char * p1 = num1, * p2 = num2;
+    int n = 0, m = 0;
+    while (*p1++ != '\0') n++;
+    while (*p2++ != '\0') m++;
+    
+    int sum = 0;
+    int maxSize = MAX(m, n);
+    char * result = malloc(sizeof(char) * (maxSize + 1));
+    memset(result, 0, maxSize + 1);
+    int k = maxSize - 1;
+    int i = n - 1, j = m - 1;
+    while (i >= 0 || j >= 0) {
+        if (i >= 0) sum += num1[i--] - '0';
+        if (j >= 0) sum += num2[j--] - '0';
+        result[k--] = sum % 10 + '0';
+        sum /= 10;
+    }
+    if (sum > 0) {
+        char * newResult = malloc(maxSize + 2);
+        memcpy(newResult + 1, result, maxSize + 1);
+        newResult[0] = sum + '0';
+        return newResult;
+    }
+    return result;
+}
+
+void testAddIntergerInArrayForm()
+{
+    int array[] = {9, 9, 1, 2};
+    int size = sizeof(array) / sizeof(array[0]);
+    
+    int k = 98;
+    int returnSize = 0;
+    int * result = addIntergerInArrayForm(array, size, k, &returnSize);
+    printf("arrayForm: ");
+    printArray(result, returnSize);
+}
+
+int * addIntergerInArrayForm(int * array, int size, int k, int * returnSize)
+{
+    if (!array || size <= 0 || !returnSize) return NULL;
+    int j = 0;
+    int m = k;
+    while (m) { m /= 10; j++;}
+    
+    int i = size - 1;
+    int maxSize = j > size ? j : size;
+    int * result = malloc(sizeof(int) * maxSize);
+    int sum = 0;
+    m = maxSize;
+    while (i >= 0 || k > 0) {
+        if (i >= 0) sum += array[i--];
+        if (k > 0) sum += k % 10;
+        result[m - 1] = sum % 10;
+        sum /= 10;
+        k /= 10;
+        m --;
+    }
+    if (sum) {
+        int * newResult = malloc(sizeof(int) * (maxSize + 1));
+        memcpy(newResult + 1, result, maxSize * sizeof(int));
+        newResult[0] = sum;
+        *returnSize = maxSize + 1;
+        return newResult;
+    }
+    *returnSize = maxSize;
+    return result;
 }
 
 @end
