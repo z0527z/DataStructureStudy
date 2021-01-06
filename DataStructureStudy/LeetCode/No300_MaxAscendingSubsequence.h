ea8dc67f5d7f2116f28a30d6507be1c7bf222264
jolin.ding <jolin.ding@corp.to8to.com>
2021-01-06 22:29:16 +0800

    增加最大上升子序列

---
+++ DataStructureStudy/LeetCode/No300_MaxAscendingSubsequence.h
@@ -0,0 +1,17 @@
+//
+//  No300_MaxAscendingSubsequence.h
+//  DataStructureStudy
+//
+//  Created by jolin.ding on 2021/1/6.
+//  Copyright © 2021 jolin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface No300_MaxAscendingSubsequence : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DataStructureStudy/LeetCode/No300_MaxAscendingSubsequence.m
@@ -0,0 +1,72 @@
+//
+//  No300_MaxAscendingSubsequence.m
+//  DataStructureStudy
+//
+//  Created by jolin.ding on 2021/1/6.
+//  Copyright © 2021 jolin. All rights reserved.
+//
+
+#import "No300_MaxAscendingSubsequence.h"
+
+@implementation No300_MaxAscendingSubsequence
+
+- (instancetype)init {
+    if (self = [super init]) {
+        int array[] = {10, 9, 2, 5, 3, 7, 101, 18};
+        int length = sizeof(array) / sizeof(array[0]);
+        
+        int subLen = maxLengthOfAscendingSubsequenceByDynamic(array, length);
+        printf("\n--------- maxLengthOfAscendingSubsequenceByDynamic --------\n\n");
+        printf("maxSum: %d\n", subLen);
+    }
+    return self;
+}
+
+#pragma mark - 动态规划
+/**
+ * 以  [ 8, 2, 3, 1, 4] 为例拆解具体的过程
+ *
+ *  1、初始化数组 dp 的每个值为1
+ *
+ *  2、
+ *      ↓
+ *      8               2               3               1            4
+ *     dp[0] = 1
+ *
+ *              ↓
+ *      8               2               3               1            4
+ *      2比8小，所以不更新dp[1], dp[1] = 1
+ *
+ *
+ *                     ↓
+ *      8               2               3               1            4
+ *      3比8小，所以不更新dp[2], 但3比2大，所以2可以和3构成一个上升的子序列， dp[2] = dp[1] + 1 = 2
+ *
+ *
+ */
+int maxLengthOfAscendingSubsequenceByDynamic(int * array, int length) {
+    if (!array || length < 1) return 0;
+    
+    int * dp = malloc(sizeof(int) * length);
+    // 初始化数组dp的每个值为1
+    for (int i = 0; i < length; i ++) {
+        dp[i] = 1;
+    }
+    
+    int maxLen = 1;
+    for (int i = 1; i < length; i ++) {
+        for (int j = 0; j < i; j ++) {
+            /**
+             * 如果当前值array[i] > array[j]，说明array[i]可以和
+             * array[j]结尾的上升序列构成一个新的上升子序列
+             */
+            if (array[i] > array[j]) {
+                dp[i] = MAX(dp[i], dp[j] + 1);
+                maxLen = MAX(maxLen, dp[i]);
+            }
+        }
+    }
+    return maxLen;
+}
+
+@end
