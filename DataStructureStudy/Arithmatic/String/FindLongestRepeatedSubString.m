352a503ab54eaf6084fbcc116982601466e0870a
jolin.ding <jolin.ding@corp.to8to.com>
2021-02-03 13:50:54 +0800

    增加后缀树实现

--- DataStructureStudy/Arithmatic/String/FindLongestRepeatedSubString.m
+++ DataStructureStudy/Arithmatic/String/FindLongestRepeatedSubString.m
@@ -14,12 +14,13 @@
     if (self = [super init]) {
         
         char * str = "aaaaabbb";
-        char * subStr = findLongestRepeatedSubString(str);
+//        char * subStr = findLongestRepeatedSubString(str);
         printf("\n---------- FindLongestRepeatedSubString ---------\n\n");
-        printf("subString:%s\n", subStr);
-        if (subStr) {
-            free(subStr);
-        }
+//        printf("subString:%s\n", subStr);
+//        if (subStr) {
+//            free(subStr);
+//        }
+        testSuffixTree(str);
     }
     return self;
 }
@@ -83,5 +84,203 @@ char * findLongestRepeatedSubString(char * str) {
 
 
 #pragma mark - 后缀树
+#define MAX_CHAR 256
+typedef struct SuffixTreeNode {
+    struct SuffixTreeNode * children[MAX_CHAR];
+    
+    struct SuffixTreeNode * suffixLink;
+    
+    int start, *end;
+    
+    int suffixIndex;
+}Node;
+
+char text[100];
+Node * root = NULL;
+
+Node * lastNewNode = NULL;
+Node * activeNode = NULL;
+int count = 0;
+
+int activeEdge = -1;
+int activeLength = 0;
+
+int remainingSuffixCount = 0;
+int leafEnd = -1;
+int * rootEnd = NULL;
+int * splitEnd = NULL;
+int size = -1;
+
+
+Node * node(int start, int * end) {
+    count++;
+    Node * node = (Node *)malloc(sizeof(Node));
+    int i;
+    for (i = 0; i < MAX_CHAR; i ++) {
+        node->children[i] = NULL;
+    }
+    
+    node->suffixLink = root;
+    node->start = start;
+    node->end = end;
+    
+    node->suffixIndex = -1;
+    return node;
+}
+
+int edgeLength(Node * n) {
+    return *(n->end) - (n->start) + 1;
+}
+
+int walkDown(Node *currNode) {
+    if (activeLength >= edgeLength(currNode)) {
+        activeEdge = (int)text[activeEdge + edgeLength(currNode)] - (int)' ';
+        activeLength -= edgeLength(currNode);
+        activeNode = currNode;
+        return 1;
+    }
+    return 0;
+}
+
+void extendSuffixTree(int pos) {
+    // rule 1
+    leafEnd = pos;
+    
+    remainingSuffixCount ++;
+    
+    lastNewNode = NULL;
+    
+    while (remainingSuffixCount > 0) {
+        if (activeLength == 0) {
+            // APCFALZ
+            activeEdge = (int)text[pos] - (int)' ';
+        }
+        
+        if (activeNode->children[activeEdge] == NULL) {
+            activeNode->children[activeEdge] = node(pos, &leafEnd);
+            
+            if (lastNewNode != NULL) {
+                lastNewNode->suffixLink = activeNode;
+                lastNewNode = NULL;
+            }
+        }
+        else {
+            Node * next = activeNode->children[activeEdge];
+            if (walkDown(next)) {
+                continue;
+            }
+            
+            if (text[next->start + activeLength] == text[pos]) {
+                if (lastNewNode != NULL && activeNode != root) {
+                    lastNewNode->suffixLink = activeNode;
+                    lastNewNode = NULL;
+                }
+                activeLength ++;
+                break;
+            }
+            
+            splitEnd = (int *)malloc(sizeof(int));
+            *splitEnd = next->start + activeLength - 1;
+            
+            Node * split = node(next->start, splitEnd);
+            activeNode->children[activeEdge] = split;
+            
+            split->children[(int)text[pos] - (int)' '] = node(pos, &leafEnd);
+            next->start += activeLength;
+            split->children[activeEdge] = next;
+            
+            if (lastNewNode != NULL) {
+                lastNewNode->suffixLink = split;
+            }
+            
+            lastNewNode = split;
+        }
+        
+        remainingSuffixCount --;
+        if (activeNode == root && activeLength > 0) {
+            activeLength --;
+            activeEdge = (int)text[pos - remainingSuffixCount + 1] - (int)' ';
+        }
+        //APCFER2C2
+        else if (activeNode != root) {
+            activeNode = activeNode->suffixLink;
+        }
+    }
+}
+
+void print(int i, int j) {
+    int k;
+    for (k = i; k <= j; k ++) {
+        printf("%c", text[k]);
+    }
+}
+
+void setSuffixIndexByDFS(Node * n, int labelHeight) {
+    if (n == NULL) return;
+    
+    if (n->start != -1) {
+        print(n->start, *(n->end));
+    }
+    int leaf = 1;
+    int i;
+    for (i = 0; i < MAX_CHAR; i ++) {
+        if (n->children[i] != NULL) {
+            if (leaf == 1 && n->start != -1) {
+                printf(" [%d]\n", n->suffixIndex);
+                
+                leaf = 0;
+                setSuffixIndexByDFS(n->children[i], labelHeight + edgeLength(n->children[i]));
+            }
+        }
+    }
+    if (leaf == 1) {
+        n->suffixIndex = size - labelHeight;
+        printf(" [%d]\n", n->suffixIndex);
+    }
+}
+
+void freeSuffixTreeByPostOrder(Node * n) {
+    if (n == NULL) return;
+    int i;
+    for (i = 0; i < MAX_CHAR; i++)
+    {
+        if (n->children[i] != NULL)
+        {
+            freeSuffixTreeByPostOrder(n->children[i]);
+        }
+    }
+    if (n->suffixIndex == -1) {
+        free(n->end);
+    }
+    free(n);
+}
+
+void buildSuffixTree() {
+    size = (int)strlen(text);
+    int i;
+    rootEnd = (int *)malloc(sizeof(int));
+    *rootEnd = -1;
+    
+    root = node(-1, rootEnd);
+    activeNode = root;
+    
+    for (i = 0; i < size; i ++) {
+        extendSuffixTree(i);
+    }
+    
+    int labelHeight = 0;
+    setSuffixIndexByDFS(root, labelHeight);
+    
+    // free
+//    freeSuffixTreeByPostOrder(root);
+}
+
+void testSuffixTree(char * str) {
+    strcpy(text, "abbc");
+    buildSuffixTree();
+}
+
+
+
 
 @end
